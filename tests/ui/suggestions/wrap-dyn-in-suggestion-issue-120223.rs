9d0fdfac5d459e9cd9be1666601c4ddc2f025c35
long-long-float <niinikazuki@yahoo.co.jp>
2024-04-21 11:42:50 +0900

    Support Sized

--- tests/ui/suggestions/wrap-dyn-in-suggestion-issue-120223.rs
+++ tests/ui/suggestions/wrap-dyn-in-suggestion-issue-120223.rs
@@ -19,4 +19,11 @@ pub fn in_ty_param<F: FnOnce() -> &'static dyn std::fmt::Debug>(f: F) {
     f(); //~ ERROR use of moved value
 }
 
+fn with_sized<T: Fn() -> &'static (dyn std::fmt::Debug) + ?Sized>() {
+    without_sized::<T>(); 
+    //~^ ERROR the size for values of type `T` cannot be known at compilation time
+}
+
+fn without_sized<T: Fn() -> &'static dyn std::fmt::Debug>() {}
+
 fn main() {}
